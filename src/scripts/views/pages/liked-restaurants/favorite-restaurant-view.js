4e0b6885cc2bebee3107b4907b6750b29c71dcaa
annamaulina654 <annamaulina14@gmail.com>
2024-12-31 18:30:30 +0700

    finishing the submission task

---
+++ src/scripts/views/pages/liked-restaurants/favorite-restaurant-view.js
@@ -0,0 +1,57 @@
+import { createRestaurantItemTemplate } from '../../templates/template-creator';
+
+class FavoriteRestaurantView {
+  getTemplate() {
+    //   return `
+    // <div class="content">
+    //   <input id="query" type="text">
+    //   <h2 class="restaurant">Your Liked Restaurant</h2>
+
+    //     <div id="restaurants" class="restaurants">
+    //     </div>
+    //   </div>
+    // </div>
+    //   `;
+    return `
+    <div class="container">
+      <h1 class="restaurant">Favorite Restaurant</h1>
+      <input id="query" type="text" placeholder="Search your favorite restaurant..." />
+      <div class="card-container" id="restaurants">
+      </div>
+    </div>
+  `;
+
+  }
+
+
+  runWhenUserIsSearching(callback) {
+    document.getElementById('query').addEventListener('change', (event) => {
+      callback(event.target.value);
+    });
+  }
+
+  showRestaurants(restaurants) {
+    this.showFavoriteRestaurants(restaurants);
+  }
+
+  showFavoriteRestaurants(restaurants) {
+    let html;
+    if (restaurants.length) {
+      html = restaurants.reduce((carry, restaurant) => `${carry  }<article class="restaurant-item">${createRestaurantItemTemplate(restaurant)}</article>`, '');
+    } else {
+      html = this._getEmptyRestaurantTemplate();
+    }
+    document.getElementById('restaurants').innerHTML = html;
+    document.getElementById('restaurants').dispatchEvent(new Event('restaurants:updated'));
+  }
+
+  _getEmptyRestaurantTemplate() {
+    return `
+          <div class="restaurant-item__not__found">
+            Tidak ada restoran untuk ditampilkan
+          </div>
+        `;
+  }
+}
+
+export default FavoriteRestaurantView;
